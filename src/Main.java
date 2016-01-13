e98644faaace7e1ef47fee58ee25417cca2a4117
Vladimir Prus <vladimir@codesourcery.com>
2016-01-13 20:39:03 +0300

    Position the Windows in the middle of the screen.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
 
 
@@ -17,6 +19,13 @@ public class Main {
 		new Content(shell);
 		
 		shell.pack();
+		Monitor primary = display.getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = shell.getBounds();
+		int x = bounds.x + (bounds.width - rect.width) / 2;
+		int y = bounds.y + (bounds.height - rect.height) / 2;
+		shell.setLocation(x, y);
+
 		shell.open();
 		
 		while (!shell.isDisposed()) {
